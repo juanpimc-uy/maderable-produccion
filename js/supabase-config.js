22145bafbe8012bdc316127278b2193662a2fcab
Juan Pablo Martinez <juanpablomartinez@192.168.1.14>
2026-04-26 18:46:19 -0300

    fix: set real Supabase anon key in supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,5 +1,5 @@
 window.SUPABASE_URL = 'https://xhfeurinovvsbgobkidy.supabase.co';
-window.SUPABASE_ANON_KEY = '__PEGAR_ANON_KEY__';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhoZmV1cmlub3Z2c2Jnb2JraWR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3ODM3MjksImV4cCI6MjA4OTM1OTcyOX0.iGYICZQxSAPV_XyahHygmAxyvaEUDA-fGNjpiO9WdRA';
 
 window.sbFetch = async function(path, opts = {}) {
   const url = `${window.SUPABASE_URL}/rest/v1/${path}`;
